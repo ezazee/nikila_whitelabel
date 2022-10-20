3dddb450d6a2f216c23d5b086d2cb40d1221c742
rumahac <saefudin.rumahac@gmail.com>
2022-10-20 16:38:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://nikilapol.com/assets/img/Ilustrasi/home-img.png" width="400"></a></p>
+
+<h1 align="center">Nikila Web Statis</h1>
+
+## 🚀 Teknologi yang dipakai:
+
+<p align="center"> 
+    <a href="#" target="_blank"> <img src="https://img.icons8.com/color/48/000000/html-5--v1.png"/> </a>
+    <a href="#" target="_blank"> <img src="https://img.icons8.com/color/48/000000/css3.png"/> </a>
+    <a href="#" target="_blank"> <img src="https://img.icons8.com/color/48/000000/javascript--v1.png"/> </a>
+</p>
+
+
+## Tentang Project
+Project Kantor untuk Kabupaten Lamongan
+
+## Cara Run Project
+
+```bash
+Klik Kanan Lalu Pilih Open With Live Server 
+
---
+++ js/video-player.js
@@ -0,0 +1,75 @@
+// const main_video = document.querySelector(".main-video video");
+// const main_video_title = document.querySelector(".main-video .title");
+// const video_playlist = document.querySelector(".video-playlist .videos");
+
+// let data = [
+//   {
+//     id: "a1",
+//     title: "Festival Pesona Selat Lembeh 2022",
+//     name: "Festival Pesona Selat Lembeh 2022.mp4",
+//     duration: "2:47",
+//   },
+//   {
+//     id: "a2",
+//     title: "Nikila Sahabat UMKM",
+//     name: "Nikila Sahabat UMKM.mp4",
+//     duration: "60:00",
+//   },
+//   {
+//     id: "a3",
+//     title: "Vlog Ekslusif TIM Nikila",
+//     name: "Vlog Ekslusif TIM Nikila.mp4",
+//     duration: "24:49",
+//   },
+// ];
+
+// data.forEach((video, i) => {
+//   let video_element = `
+//                 <div class="video" data-id="${video.id}">
+//                     <img src="assets/img/play.svg" alt="">
+//                     <p>${i + 1 > 9 ? i + 1 : "0" + (i + 1)}. </p>
+//                     <h3 class="title">${video.title}</h3>
+//                     <p class="time">${video.duration}</p>
+//                 </div>
+//     `;
+//   video_playlist.innerHTML += video_element;
+// });
+
+// let videos = document.querySelectorAll(".video");
+// videos[0].classList.add("active");
+// videos[0].querySelector("img").src = "assets/img/pause.svg";
+
+// videos.forEach((selected_video) => {
+//   selected_video.onclick = () => {
+//     for (all_videos of videos) {
+//       all_videos.classList.remove("active");
+//       all_videos.querySelector("img").src = "assets/img/play.svg";
+//     }
+
+//     selected_video.classList.add("active");
+//     selected_video.querySelector("img").src = "assets/img/pause.svg";
+
+//     let match_video = data.find(
+//       (video) => video.id == selected_video.dataset.id
+//     );
+//     main_video.src = "assets/videos/" + match_video.name;
+//     main_video_title.innerHTML = match_video.title;
+//   };
+// });
+
+// let listVideo = document.querySelectorAll(".video-list .vid");
+// let mainVideo = document.querySelector(".main-video .video");
+// let title = document.querySelectorAll(".main-video .title-galery");
+
+// listVideo.forEach((video) => {
+//   video.onclick = () => {
+//     listVideo.forEach((vid) => vid.classList.remove("active"));
+//     video.classList.add("active");
+//     if (video.classList.contains("active")) {
+//       let src = video.children[0].getAttribute("src");
+//       mainVideo.src = src;
+//       let text = video.children[1].innerHTML;
+//       title.innerHTML = text;
+//     }
+//   };
+// });
